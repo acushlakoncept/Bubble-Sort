d82297c3073d44ac1d5ee0179e3f615b580bd503
Uduak Essien <acushla4real@gmail.com>
2020-06-11 17:03:57 +0100

    finish the bubble sort algorithm

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,15 +1,20 @@
-def bubble_sort(arr)
-    i = 0
-    j = 1
-    while i < arr.length do
-        if arr[i] < arr[j]
-        i += 1
-        j += 1
-        elsif arr[i] > arr[j]
-        i = 0
-        j = 1
+def bubble_sort(arr) 
+    swapTest = true
 
+    for i in (0...arr.length)
+       for j in (0...arr.length-1)
+        # swap
+        if arr[j] > arr[j+1]
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+
+            swapTest = false
+        end
+       end
+       break if swapTest
     end
-end 
+    return arr
+end
 
-bubble_sort([1,3,5,6,3,4,34,6])
+puts bubble_sort(arr)
