c8a7fb16416f54443f1a9bfef744b6cd7469280a
Uduak Essien <acushla4real@gmail.com>
2020-06-11 17:20:27 +0100

    adjust the indentation

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,20 +1,21 @@
-def bubble_sort(arr) 
-    swapTest = true
+def bubble_sort(arr)
+  swapTest = true
+  
+  for i in (0...arr.length)
+    for j in (0...arr.length-1)
+     # swap
+     if arr[j] > arr[j+1]
+         temp = arr[j]
+         arr[j] = arr[j+1]
+         arr[j+1] = temp
 
-    for i in (0...arr.length)
-       for j in (0...arr.length-1)
-        # swap
-        if arr[j] > arr[j+1]
-            temp = arr[j]
-            arr[j] = arr[j+1]
-            arr[j+1] = temp
-
-            swapTest = false
-        end
-       end
-       break if swapTest
+         swapTest = false
+     end
     end
-    return arr
+    break if swapTest
+ end
+ return arr
+    
 end
 
 puts bubble_sort(arr)
