4e6c4507d495cb260615c77a935f0d4d3b99dbe1
jacobrees <jacob.rees97@yahoo.com>
2020-06-12 13:02:27 +0100

    Fix yield in jacob bubble sort file, add zero argument to fix infinite loop

--- jacob_bubblesort.rb
+++ jacob_bubblesort.rb
@@ -12,4 +12,25 @@ def bubble_sort(arr)
   arr
 end
 
-print bubble_sort([1, 2, 5, 3, 6, 7, 4, 3])
+def bubble_sort_by(arr)
+  i = 0
+  arr_length = arr.length - 1
+  while i < arr_length
+    if yield(arr[i], arr[i + 1]).negative?
+      i += 1
+    elsif yield(arr[i], arr[i + 1]).zero?
+      i += 1
+    elsif yield(arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      i = 0
+    end
+
+  end
+  print arr
+end
+
+print bubble_sort([1, 2, 5, 3, 6, 5, 6, 6, 7, 4, 3])
+
+bubble_sort_by %w[i i hi hello hey] do |left,right|
+  left.length - right.length
+end
