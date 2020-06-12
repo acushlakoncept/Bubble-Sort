b0a09b25e72d0b0c74e4fb0a107b0c662104a2a1
Uduak Essien <acushla4real@gmail.com>
2020-06-12 09:42:44 +0100

    update on rubocop error fix

--- uduak_bubblesort.rb
+++ uduak_bubblesort.rb
@@ -1,24 +1,19 @@
 def bubble_sort(arr)
-  swapTest = true
-  
+  swap_test = true
   for i in (0...arr.length)
-    for j in (0...arr.length-1)
-     
-     if arr[j] > arr[j+1]       
-         # swap
-         temp = arr[j]
-         arr[j] = arr[j+1]
-         arr[j+1] = temp
+    for j in (0...arr.length - 1)
+      if arr[j] > arr[j + 1]
+        # swap
+        temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
 
-         swapTest = false
-     end
+        swap_test = false
+      end
     end
-    break if swapTest
- end
-
- return arr
-    
+    break if swap_test
+  end
+  arr
 end
 
 print bubble_sort([200, 6, 8, 9, 1, 56, 23, 1, 8])
-puts ' '
