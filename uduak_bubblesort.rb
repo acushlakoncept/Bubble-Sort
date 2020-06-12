fcbefc472f44c340a0169ed4435df3413b83daa8
Uduak Essien <acushla4real@gmail.com>
2020-06-12 10:20:30 +0100

    add sort_by algorithm

--- uduak_bubblesort.rb
+++ uduak_bubblesort.rb
@@ -15,4 +15,29 @@ def bubble_sort(arr)
   arr
 end
 
+
+def bubble_sort_by(arr)
+  swap_test = true
+  (0...arr.length).each do |_i|
+    (0...arr.length - 1).each do |j|
+      next unless (yield arr[j], arr[j + 1]).positive?
+
+      temp = arr[j]
+      arr[j] = arr[j + 1]
+      arr[j + 1] = temp
+      swap_test = false
+    end
+    break if swap_test
+  end
+
+  arr
+end
+
+
+bsort_by = bubble_sort_by(%w[hello hey hi here]) do |a, b|
+  a.length - b.length
+end
+
+
 p bubble_sort([200, 6, 8, 9, 1, 56, 23, 1, 8])
+p bsort_by
